2ff5bbc61d4e6729e59569b8dc7f5ee441fe1be1
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-22 13:27:13 +0300

    refactor: Update CourseDetail page layout and content

--- src/app_frontend/src/pages/CourseDetail.tsx
+++ src/app_frontend/src/pages/CourseDetail.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
-import learningModules from "../utils/learningModules";
+import learningModules, { TLearningModule } from "../utils/learningModules";
 import { Button } from "../../@/components/ui/button";
 import { FaChevronLeft } from "react-icons/fa";
 import { SidebarHeader } from "../components/Sidebar";
+import { set } from "date-fns";
 
 function CourseDetail() {
   const { id } = useParams<{ id: string }>();
   const navigate = useNavigate();
+  const [currentCourse, setCurrentCourse] = React.useState<
+    TLearningModule | undefined
+  >(undefined);
 
   const handleNavigate = () => {
     navigate("/dashboard");
@@ -40,9 +44,14 @@ function CourseDetail() {
           <SidebarHeader title="My Courses" />
         </div>
       </div>
-      <h1 className="font-heading font-bold text-3xl p-2">
-        Course {id} Detail
-      </h1>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="font-heading text-center text-xl p-2 mt-12">
+          Course will be available soon
+        </h1>
+        <Button className="text-white font-body mt-4" onClick={handleNavigate}>
+          Back Home
+        </Button>
+      </div>
     </div>
   );
 }
