556f8f108dea026684ea2e8889f4744c549966ac
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-22 13:30:52 +0300

    feat: Add development notice to Proposals page

--- src/app_frontend/src/pages/Proposals.tsx
+++ src/app_frontend/src/pages/Proposals.tsx
@@ -94,6 +94,9 @@ function Proposals() {
           Load More
         </button>
       </div>
+      <p className="py-2 italic font-body text-gray-500 text-sm text-center">
+        *This page is still in development
+      </p>
     </div>
   );
 }
