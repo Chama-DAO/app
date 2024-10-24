961e2f2d797cefccf5f6250bb724329339930e14
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-10-24 09:47:14 +0300

    refactor: Comment out unused code in index.ts

--- src/app_backend/src/index.ts
+++ src/app_backend/src/index.ts
@@ -1,7 +1,7 @@
-import { Canister, query, text } from 'azle';
+// import { Canister, query, text } from 'azle';
 
-export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
-})
+// export default Canister({
+//     greet: query([text], text, (name) => {
+//         return `Hello, ${name}!`;
+//     })
+// })
